7eea5ef78b9683962b72f5a99134eb2bbd28349e
Thomas Le <le.thomas.vu@gmail.com>
2018-01-10 13:21:12 -0800

    Changed to valid secret key sample for production

--- bloggerfun/settings.py
+++ bloggerfun/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Whatever You Want. Just make it better than this."
+SECRET_KEY = "secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
