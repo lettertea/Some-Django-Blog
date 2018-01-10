6fb69a502aa02df504e85933e77f07947aa33807
Thomas Le <le.thomas.vu@gmail.com>
2018-01-10 13:36:43 -0800

    settings

--- bloggerfun/settings.py
+++ bloggerfun/settings.py
@@ -116,7 +116,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://some-coding-blog.herokuapp.com/']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
